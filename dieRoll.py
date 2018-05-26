9cc5c5027c5884f62c55548f7f510565a140a015
sarahoka <39632861+sarahoka@users.noreply.github.com>
2018-05-25 18:12:58 -0700

    Update dieRoll.py

--- dieRoll.py
+++ dieRoll.py
@@ -11,14 +11,17 @@ class dieRoll:
     # die roll command
     @commands.command()
     async def die_roll(self, ctx):
-        await ctx.send("You selected 'die_roll'! How many sides is your die?")
-        await ctx.send("Please enter any positive integer.")
-        sides = ctx.message.content.split(" ")
-        await ctx.send(sides)
-        index = int(sides[1])
-        x = random.randint(1, index)
-        await ctx.send("Rolling...")
-        await ctx.send(x)
+        try:
+            await ctx.send("You selected 'die_roll' ! How many sides is your die?")
+            await ctx.send("Please enter any positive integer.")
+            sides = ctx.message.content.split(" ")
+            await ctx.send(sides)
+            index = int(sides[1])
+            x = random.randint(1, index)
+            await ctx.send("Rolling...")
+            await ctx.send(x)
+        except Exception:
+            await ctx.send("Error! Please enter a positive integer!")
 
 # add this cog to the bot
 def setup(bot):

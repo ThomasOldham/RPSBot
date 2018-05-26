0fc7904ff0a51d19ca13c1c36b63166d9413797f
sarahoka <39632861+sarahoka@users.noreply.github.com>
2018-05-25 18:06:29 -0700

    Add files via upload

---
+++ dieRoll.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import math
+import random
+from random import randint
+
+class dieRoll:
+    def __init__(self, bot):
+        self.bot = bot
+
+    # die roll command
+    @commands.command()
+    async def die_roll(self, ctx):
+        await ctx.send("You selected 'die_roll'! How many sides is your die?")
+        await ctx.send("Please enter any positive integer.")
+        sides = ctx.message.content.split(" ")
+        await ctx.send(sides)
+        index = int(sides[1])
+        x = random.randint(1, index)
+        await ctx.send("Rolling...")
+        await ctx.send(x)
+
+# add this cog to the bot
+def setup(bot):
+    bot.add_cog(dieRoll(bot))
